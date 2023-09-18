badbd9044ae7c7e1d1d10b015e2122d36081b222
Joni Hackzell <t1hajo05@students.oamk.fi>
2023-09-18 22:20:29 +0300

    Background color test

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <StatusBar style="auto" 
+      backgroundColor='orange'
+      //hidden={true}
+      />
+      </SafeAreaView>
   );
 }
 
@@ -14,7 +18,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? Constants.statusBarHeight: 0,
+    //alignItems: 'center',
+    //justifyContent: 'center',
   },
 });
